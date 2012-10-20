6c23e7b3f0756a20545c18707b3dfd34bd8748b7
Malte <werbung.fuermich@gmx.de>
2012-10-20 12:16:11 +0200

    Delete

--- RecWar/HelloWorldLayer.h
+++ RecWar/HelloWorldLayer.h
@@ -13,8 +13,7 @@
 #import "cocos2d.h"
 
 // HelloWorldLayer
-@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
-{
+@interface HelloWorldLayer : CCLayer{
 }
 
 // returns a CCScene that contains the HelloWorldLayer as the only child
